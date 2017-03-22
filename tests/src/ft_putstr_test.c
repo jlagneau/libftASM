d5851728289c99dfca661fd64f06ce52787cf9a1
jlagneau <jlagneau@student.42.fr>
2017-03-22 17:02:47 +0100

    Revert test

--- tests/src/ft_putstr_test.c
+++ tests/src/ft_putstr_test.c
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/22 09:14:42 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/03/22 16:55:31 by jlagneau         ###   ########.fr       */
+/*   Updated: 2017/03/22 17:02:11 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,14 @@
 int		ft_putstr_test(void)
 {
 	int		fd;
-	int		ret;
 
 	fd = close_stdout();
 	if (fd < 0)
 		return (-1);
 	if (0 != ft_putstr(0))
 		return (1);
-	else if (0 != (ret = ft_putstr("")))
-		return (ret);
+	else if (0 != ft_putstr(""))
+		return (2);
 	else if (14 != ft_putstr("Hello, World !"))
 		return (3);
 	reopen_stdout(fd);
