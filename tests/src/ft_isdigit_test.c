8d61140ee51e5c7b979f7fdd4ca6493bbfdaf43f
jlagneau <jlagneau@student.42.fr>
2017-03-31 00:52:19 +0200

    Add ft_isdigit

--- tests/src/ft_isdigit_test.c
+++ tests/src/ft_isdigit_test.c
@@ -6,11 +6,23 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 08:32:59 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/03/23 08:33:15 by jlagneau         ###   ########.fr       */
+/*   Updated: 2017/03/31 00:07:46 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_isdigit_test(void)
+#include <libft.h>
+#include "test.h"
+
+int			ft_isdigit_test(void)
 {
-	return (1);
+	int		fails;
+
+	fails = g_fails;
+	ASSERT(1 == ft_isdigit('0'))
+	ASSERT(1 == ft_isdigit('9'))
+	ASSERT(1 == ft_isdigit('5'))
+	ASSERT(0 == ft_isdigit('!'))
+	ASSERT(0 == ft_isdigit('*'))
+	ASSERT(0 == ft_isdigit('\n'))
+	return (g_fails - fails);
 }
