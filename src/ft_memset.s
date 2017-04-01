b99a5e216a249d2c94be183dcf3fabc90ec3a7a8
jlagneau <jlagneau@student.42.fr>
2017-04-01 03:59:28 +0200

    Add ft_memset

---
+++ src/ft_memset.s
@@ -0,0 +1,31 @@
+;******************************************************************************;
+;                                                                              ;
+;                                                         :::      ::::::::    ;
+;    ft_memset.s                                        :+:      :+:    :+:    ;
+;                                                     +:+ +:+         +:+      ;
+;    By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+         ;
+;                                                 +#+#+#+#+#+   +#+            ;
+;    Created: 2017/03/31 05:22:36 by jlagneau          #+#    #+#              ;
+;    Updated: 2017/04/01 01:27:07 by jlagneau         ###   ########.fr        ;
+;                                                                              ;
+;******************************************************************************;
+
+%include    "define/define.s"
+
+section     .text
+    global  sym(ft_memset)      ; void *ft_memset(void *s, int c, size_t n)
+
+sym(ft_memset):
+    nop
+
+    push    rdi                 ; store s ptr
+
+    mov     rcx, rdx            ; rcx = n
+    mov     rax, rsi            ; rax = c
+
+    cld                         ; clear direction flag
+    rep     stosb               ; store al (rax) in rdi and increment rdi ptr
+                                ; until rcx reach 0
+    pop     rdi                 ; return s ptr
+    mov     rax, rdi
+    ret
