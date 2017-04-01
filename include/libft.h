59f5c0712a522d91f3d6ca7075d7ed09c9d188a7
jlagneau <jlagneau@student.42.fr>
2017-04-01 04:02:29 +0200

    Add lot of functions

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/24 07:15:31 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/03/31 01:51:40 by jlagneau         ###   ########.fr       */
+/*   Updated: 2017/04/01 02:38:11 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define LIBFT_H
 # include <string.h>
 
+void	ft_bzero(void *s, size_t n);
+char	*ft_strcat(char *d, const char *s);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -21,17 +23,25 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_puts(char *s);
+int		ft_puts(const char *s);
 
-size_t	ft_strlen(char *s);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memcpy(void *d, const void *s, size_t n);
+char	*ft_strdup(const char *s);
 
 int		ft_putchar(char c);
 int		ft_putchar_fd(char c, int fd);
-int		ft_putstr(char *s);
-int		ft_putstr_fd(char *s, int fd);
+int		ft_putstr(const char *s);
+int		ft_putstr_fd(const char *s, int fd);
 int		ft_putnbr_u(unsigned int n);
 int		ft_putnbr_u_fd(unsigned int n, int fd);
-int		ft_putendl(char *s);
-int		ft_putendl_fd(char *s, int fd);
+int		ft_putendl(const char *s);
+int		ft_putendl_fd(const char *s, int fd);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strcpy(char *d, const char *s);
+char	*ft_strnew(size_t n);
+char	*ft_strrepl(char *s, int (*f)(int));
 
 #endif
