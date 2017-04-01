ea2a426597e507e09920373ea5d565fdadd65bcb
jlagneau <jlagneau@student.42.fr>
2017-04-01 18:40:18 +0200

    Fix long long error in Travis-CI for C90 in GCC Linux

--- tests/src/ft_putnbr_test.c
+++ tests/src/ft_putnbr_test.c
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 08:53:10 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/04/01 18:27:02 by jlagneau         ###   ########.fr       */
+/*   Updated: 2017/04/01 18:38:29 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@
 ** -9223372036854775808 says that it's too large for a signed value so I cannot
 ** test it other than by puting it into a long long variable, can I?
 */
-int				ft_putnbr_test(void)
+int			ft_putnbr_test(void)
 {
-	int			fd;
-	int			fails;
-	long long	test;
+	int		fd;
+	int		fails;
+	long	test;
 
 	fd = close_stdout();
 	fails = g_fails;
@@ -37,15 +37,18 @@ int				ft_putnbr_test(void)
 	ASSERT(1 == ft_putnbr(7));
 	ASSERT(2 == ft_putnbr(42));
 	ASSERT(10 == ft_putnbr(2147483647));
-	ASSERT(10 == ft_putnbr(3000000000));
-	ASSERT(10 == ft_putnbr(4294967295));
-	ASSERT(19 == ft_putnbr(9223372036854775807));
 	ASSERT(2 == ft_putnbr(-1));
 	ASSERT(11 == ft_putnbr(-2147483648));
-	test = -9223372036854775807;
-	ASSERT(20 == ft_putnbr(test));
-	test--;
-	ASSERT(20 == ft_putnbr(test));
+	if (sizeof(long) == 8)
+	{
+		test = -9223372036854775807;
+		ASSERT(10 == ft_putnbr(3000000000));
+		ASSERT(10 == ft_putnbr(4294967295));
+		ASSERT(19 == ft_putnbr(9223372036854775807));
+		ASSERT(20 == ft_putnbr(test));
+		test--;
+		ASSERT(20 == ft_putnbr(test));
+	}
 	reopen_stdout(fd);
 	return (g_fails - fails);
 }
