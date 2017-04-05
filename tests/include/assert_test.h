15ce9b5df9b20e18473d8955ccc840c5c15147ec
jlagneau <jlagneau@student.42.fr>
2017-04-05 22:55:12 +0200

    Fix tests return value for ft_cat and add some verification on opened file

--- tests/include/assert_test.h
+++ tests/include/assert_test.h
@@ -6,15 +6,16 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/29 16:01:51 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/03/29 16:25:25 by jlagneau         ###   ########.fr       */
+/*   Updated: 2017/04/05 09:59:37 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASSERT_TEST_H
 # define ASSERT_TEST_H
 
-# define PRINTL(x)	if (0 > ft_puts(x)) return (-1)
-# define PRINT(x)	if (0 > ft_putstr(x)) return (-1)
+# define PRINTL(x)	if (1) {if (0 > ft_puts(x)) return (-1);}
+# define PRINT(x)	if (1) {if (0 > ft_putstr(x)) return (-1);}
+# define PRINTR(x)	if (1) {if (0 > ft_putendl_fd(x, 2)) return (-1);}
 # define ASSERT(x)	if (1) {g_asserts++; if (!(x)) g_fails++;}
 
 int					g_asserts;
--- tests/src/ft_cat_test.c
+++ tests/src/ft_cat_test.c
@@ -6,34 +6,51 @@
 /*   By: jlagneau </var/spool/mail/jlagneau>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/02 20:17:46 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/04/02 21:49:08 by jlagneau         ###   ########.fr       */
+/*   Updated: 2017/04/05 09:58:51 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <libft.h>
+#include "test.h"
 
-int			ft_cat_test(const char *s)
+static int			verify_fd(int fd, const char *s)
 {
-	int		ret;
-	int		fd;
-	char	tmp[4096];
+	struct stat		buf;
+	char			tmp[4096];
+
+	ft_bzero(tmp, 4096);
+	(void)ft_strcat(tmp, "ft_cat: ");
+	(void)ft_strcat(tmp, s);
+	if (fd == -1 || fstat(fd, &buf) != 0)
+	{
+		perror(tmp);
+		return (1);
+	}
+	if (S_ISDIR(buf.st_mode))
+	{
+		PRINTR(ft_strcat(tmp, ": Is a directory"));
+		return (1);
+	}
+	return (0);
+}
+
+int					ft_cat_test(const char *s)
+{
+	int				ret;
+	int				fd;
 
 	if (s == 0)
 		ret = ft_cat(0);
 	else
 	{
 		fd = open(s, O_RDONLY);
-		if (fd == -1)
-		{
-			ft_bzero(tmp, 4096);
-			(void)ft_strcat(tmp, "ft_cat: ");
-			(void)ft_strcat(tmp, s);
-			perror(tmp);
-			return (-1);
-		}
+		if (verify_fd(fd, s))
+			return (1);
 		ret = ft_cat(fd);
 		close(fd);
 	}
